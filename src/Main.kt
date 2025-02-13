88054a12e97565d9ff85ef6bf5e2470c297fed9f
Lejunn100 <lejunsak@gmail.com>
2025-02-12 21:39:58 -0300

    Primeiro dia

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
-fun main() {
+fun main(args: Array<String>) {
     println("Hello World!")
+    println("Program arguments: ${args.joinToString()}")
 }
---
+++ src/teste.kt
@@ -0,0 +1,21 @@
+ fun main(args: Array<String>) {
+     var nomeFaculdade = "FIAP"
+
+     println(nomeFaculdade)
+
+     nomeFaculdade = "Faculdade de Informática e Administração Paulista"
+
+     println(nomeFaculdade)
+
+     var idade: Byte = 21
+
+     println(idade)
+
+     val pi = 3.14
+
+     var peso: Byte = -128
+
+     println(peso)
+
+     println(Byte.MAX_VALUE)
+ }
